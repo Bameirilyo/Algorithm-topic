b490c962549771fed192c6e949f6d61fb4dbb0c6
bameirilyo <1218702044@qq.com>
2018-09-27 16:34:27 +0800

    021

--- README.md
+++ README.md
@@ -23,6 +23,8 @@
 
 [018-删除链表中的节点（删除链表中的重复节点）](https://github.com/Bameirilyo/Algorithm-topic/tree/master/%E5%89%91%E6%8C%87offer/018-%E5%88%A0%E9%99%A4%E9%93%BE%E8%A1%A8%E4%B8%AD%E7%9A%84%E8%8A%82%E7%82%B9%EF%BC%88%E5%88%A0%E9%99%A4%E9%93%BE%E8%A1%A8%E4%B8%AD%E7%9A%84%E9%87%8D%E5%A4%8D%E8%8A%82%E7%82%B9%EF%BC%89)
 
+[021-调整数组顺序使奇数位于偶数前面]()
+
 [022-链表中倒数第k个节点](https://github.com/Bameirilyo/Algorithm-topic/tree/master/%E5%89%91%E6%8C%87offer/022-%E9%93%BE%E8%A1%A8%E4%B8%AD%E5%80%92%E6%95%B0%E7%AC%ACk%E4%B8%AA%E8%8A%82%E7%82%B9)
 
 [024-反转链表](https://github.com/Bameirilyo/Algorithm-topic/tree/master/%E5%89%91%E6%8C%87offer/024-%E5%8F%8D%E8%BD%AC%E9%93%BE%E8%A1%A8)
@@ -31,7 +33,7 @@
 
 [030-包含min函数的栈](https://github.com/Bameirilyo/Algorithm-topic/tree/master/%E5%89%91%E6%8C%87offer/030-%E5%8C%85%E5%90%ABmin%E5%87%BD%E6%95%B0%E7%9A%84%E6%A0%88)
 
-[042-连续子数组的最大和]()
+[042-连续子数组的最大和](https://github.com/Bameirilyo/Algorithm-topic/tree/master/%E5%89%91%E6%8C%87offer/042-%E8%BF%9E%E7%BB%AD%E5%AD%90%E6%95%B0%E7%BB%84%E7%9A%84%E6%9C%80%E5%A4%A7%E5%92%8C)
 
 ## 第二部分：经典算法Java实现
 [排序算法](https://github.com/Bameirilyo/Algorithm-topic/tree/master/%E7%BB%8F%E5%85%B8%E7%AE%97%E6%B3%95/%E6%8E%92%E5%BA%8F%E7%AE%97%E6%B3%95)
---
+++ 剑指offer/021-调整数组顺序使奇数位于偶数前面/N021.java
@@ -0,0 +1,52 @@
+public class N021 {
+    /**
+     * 法一，类似于快排，实现可扩展
+     * @param arr
+     */
+    private static void Reorder(int[] arr){
+        if (arr == null){
+            return ;
+        }
+        int left = 0;
+        int right = arr.length - 1;
+        while ( left < right){
+            //向右移动指针，直到指向偶数
+            while (left < right && !isEven(arr[left])){
+                left++;
+            }
+            //向左移动指针，直到指向奇数
+            while (left < right && !isEven(arr[right])){
+                 right--;
+            }
+            if (left < right){
+                int temp = arr[right];
+                arr[right] = arr[left];
+                arr[left] = temp;
+            }
+            if (left >= right){
+                break;
+            }
+        }
+    }
+    //判断是否偶数，实现代码解耦
+    private static boolean isEven(int n){
+        return (n & 0x1) == 0;
+    }
+
+
+    /**
+     * 法二，类似于冒泡
+     * @param arr
+     */
+    public void reOrderArray(int[] arr){
+        for (int i = 0; i < arr.length; i++){
+            for (int j = 0; j < arr.length - i - 1; j++){
+                if (arr[j] % 2 == 0 && arr[j + 1] % 2 == 1){
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
